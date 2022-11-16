4ddb79b3997e96ee299f63145626926544786e97
Jonathan Whitmire <jkwhitmi@gmail.com>
2022-11-16 14:24:45 -0600

    fixed an error occuring from stringify method in fetch request

--- src/main/webApp/login-webpage/login-script.js
+++ src/main/webApp/login-webpage/login-script.js
@@ -11,7 +11,7 @@ loginButton.addEventListener("click", (event) => {
   console.log(password.value);
   fetch(`http://localhost:8080/login`, {
     method: "POST",
-    body: JSON.stringify( username, password ),
+    body: `{"username":"${username.value}","password":"${password.value}"}`,
     credentials: "include",
   })
     .then((res) => {
@@ -22,22 +22,22 @@ loginButton.addEventListener("click", (event) => {
     });
 });
 
-// let attempt = 3;
-// function validate() {
-//     if (username == "john_doe" && password == "pass1234") {
-//         window.location = "placeholder.html";
-//         return false;
-//     } else {
-//         attempt--;
-//         alert("Invalid login information. You have " + attempt + " attempts remaining;");
-//         if (attempt == 0) {
-//             username.disabled = true;
-//             password.disabled = true;
-//             loginButton.disabled = true;
-//             return false;
-//         }
-//     }
-// }
+ let attempt = 3;
+ function validate() {
+     if (username == "john_doe" && password == "pass1234") {
+         window.location = "placeholder.html";
+         return false;
+     } else {
+         attempt--;
+         alert("Invalid login information. You have " + attempt + " attempts remaining;");
+         if (attempt == 0) {
+             username.disabled = true;
+             password.disabled = true;
+             loginButton.disabled = true;
+             return false;
+         }
+     }
+ }
 
 let rememberMeCheck;
 let guestButton;
