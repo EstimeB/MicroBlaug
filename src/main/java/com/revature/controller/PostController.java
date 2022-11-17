27ab6eb0756f4f87cb56b35eee740ef2e30a3edf
EstimeB <101056987+EstimeB@users.noreply.github.com>
2022-11-16 20:05:02 -0500

    updated dashboard js, html, & css for post feature. Got posts specific to users to render on their dashboard & now users are able to successfully create a post on their dashboard. And created a new js file & added icon folder with new icons.

--- src/main/java/com/revature/controller/PostController.java
+++ src/main/java/com/revature/controller/PostController.java
@@ -1,103 +1,110 @@
 package com.revature.controller;
 
+import com.revature.dto.Message;
 import com.revature.exception.PostNotFoundException;
 import com.revature.exception.PostUnsuccessfullyCreated;
 import com.revature.model.Post;
+import com.revature.model.User;
 import com.revature.service.PostService;
 import io.javalin.Javalin;
+import jakarta.servlet.http.HttpSession;
 
 import java.util.List;
 
 public class PostController implements Controller {
 
-        private PostService postService = new PostService();
+    private PostService postService = new PostService();
 
-        public void mapEndPoints(Javalin app) {
+    public void mapEndPoints(Javalin app) {
 
-            //create post
-            app.post("/dashboard", (ctx) -> {
-                //User user = ()
-                Post post = ctx.bodyAsClass(Post.class);
+        //create post
+        app.post("/dashboard", (ctx) -> {
+            HttpSession httpSession = ctx.req().getSession();
+            User user = (User) httpSession.getAttribute("user_info");
+            Post post = ctx.bodyAsClass(Post.class);
+            if (user == null) {
+                ctx.json(new Message("Not logged in!"));
+                ctx.status(401); // Unauthorized
+            } else {
                 try {
-                    Post createdPost = postService.createPost(post.getPostTitle(), post.getPostDescription(), post.getUserId());
+                    Post createdPost = postService.createPost(post.getPostTitle(), post.getPostDescription(), user.getUser_id());
+                    ctx.status(201); // created
                     ctx.json(createdPost);
-                    ctx.status(200);
                 } catch (IllegalArgumentException | PostUnsuccessfullyCreated e) {
                     ctx.result(e.getMessage());
-                    ctx.status(400);
+                    ctx.status(400); //Bad Request
                 }
-            });
-            //get by id
-            app.get("/posts/{id}", (ctx) -> {
-                String postId = ctx.pathParam("id");
+            }
+        });
 
-                try {
-                    int pId = Integer.parseInt(postId);
-                    Post post = postService.getPostsById(pId);
-                    ctx.json(post);
-                } catch (NumberFormatException e) {
-                    ctx.result("Id " + postId + " must be a valid integer");
-                    ctx.status(400); // 400 BAD REQUEST
-                } catch (PostNotFoundException e) {
-                    ctx.result(e.getMessage());
-                    ctx.status(404);
-                }
-            });
+        //get by id
+        app.get("/posts/{id}", (ctx) -> {
+            String postId = ctx.pathParam("id");
 
-            //get post by user id
-            app.get("/{userId}/posts", (ctx) ->{
-                String userId = ctx.pathParam("userId");
+            try {
+                int pId = Integer.parseInt(postId);
+                Post post = postService.getPostsById(pId);
+                ctx.json(post);
+            } catch (NumberFormatException e) {
+                ctx.result("Id " + postId + " must be a valid integer");
+                ctx.status(400); // 400 BAD REQUEST
+            } catch (PostNotFoundException e) {
+                ctx.result(e.getMessage());
+                ctx.status(404);
+            }
+        });
 
-                try {
-                    int uId = Integer.parseInt(userId);
-                    List<Post> posts = postService.getAllPostsBelongingToUser(uId);
-                    ctx.json(posts);
-                    ctx.status(200);
-                } catch (NumberFormatException e) {
-                    ctx.result("Id "+userId+" must be a valid int!");
-                    ctx.status(400);
-                }
-            });
-
-            //get all posts
-            app.get("/posts", (ctx) -> {
-                List<Post> posts = postService.getAllPosts();
-
+        //get post by user id
+        app.get("/userPosts", (ctx) ->{
+            HttpSession httpSession = ctx.req().getSession();
+            User user = (User) httpSession.getAttribute("user_info");
+            if (user == null) {
+                ctx.json(new Message("Not logged in!"));
+                ctx.status(401);
+            } else {
+                List<Post> posts = postService.getAllPostsBelongingToUser(user.getUser_id());
                 ctx.json(posts);
-            });
+            }
+        });
 
-            //update post
-            app.put("/posts/{id}", (ctx) -> {
-                //method is currently creating new posts
-                //need to work in having it get existing post then update it
-                Post updatePost = ctx.bodyAsClass(Post.class);
-                try {
-                    postService.updatePost(updatePost.getId(),
-                            updatePost.getPostTitle(), updatePost.getPostDescription(), updatePost.getUserId());
-                    ctx.json(updatePost);
-                    ctx.status(200);
-                } catch (IllegalArgumentException | PostUnsuccessfullyCreated e) {
-                    ctx.result(e.getMessage());
-                    ctx.status(400);
-                }
-            });
+        //get all posts
+        app.get("/posts", (ctx) -> {
+            List<Post> posts = postService.getAllPosts();
+            ctx.json(posts);
+        });
 
-            //delete post
-            app.delete("/posts/{id}", (ctx) -> {
-                String postId = ctx.pathParam("id");
+        //update post
+        app.put("/posts/{id}", (ctx) -> {
+            //method is currently creating new posts
+            //need to work in having it get existing post then update it
+            Post updatePost = ctx.bodyAsClass(Post.class);
+            try {
+                postService.updatePost(updatePost.getId(),
+                        updatePost.getPostTitle(), updatePost.getPostDescription(), updatePost.getUserId());
+                ctx.json(updatePost);
+                ctx.status(200);
+            } catch (IllegalArgumentException | PostUnsuccessfullyCreated e) {
+                ctx.result(e.getMessage());
+                ctx.status(400);
+            }
+        });
 
-                try {
-                    int pId = Integer.parseInt(postId);
-                    int deletePost = postService.deletePost(pId);
-                    ctx.json(deletePost);
-                } catch (NumberFormatException e) {
-                    ctx.result("Id " + postId + " must be a valid integer");
-                    ctx.status(400); // 400 BAD REQUEST
-                } catch (PostNotFoundException e) {
-                    ctx.result(e.getMessage());
-                    ctx.status(404);
-                }
-            });
+        //delete post
+        app.delete("/posts/{id}", (ctx) -> {
+            String postId = ctx.pathParam("id");
 
-        }
+            try {
+                int pId = Integer.parseInt(postId);
+                int deletePost = postService.deletePost(pId);
+                ctx.json(deletePost);
+            } catch (NumberFormatException e) {
+                ctx.result("Id " + postId + " must be a valid integer");
+                ctx.status(400); // 400 BAD REQUEST
+            } catch (PostNotFoundException e) {
+                ctx.result(e.getMessage());
+                ctx.status(404);
+            }
+        });
+
+    }
 }
