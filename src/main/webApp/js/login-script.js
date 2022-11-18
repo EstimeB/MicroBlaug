7ab38b49160c523e2b7e0a83190f2699c95b9239
Jonathan Whitmire <jkwhitmi@gmail.com>
2022-11-18 13:24:19 -0600

    fixed a minor backend bug

--- src/main/webApp/js/login-script.js
+++ src/main/webApp/js/login-script.js
@@ -1,5 +1,3 @@
-//const baseUrl = "http://localhost:8080";
-
 const username = document.getElementById("username");
 const password = document.getElementById("password");
 
@@ -22,22 +20,22 @@ loginButton.addEventListener("click", (event) => {
     })
 });
 
- let attempt = 3;
- function validate() {
-     if (username == "john_doe" && password == "pass1234") {
-         window.location = "placeholder.html";
-         return false;
-     } else {
-         attempt--;
-         alert("Invalid login information. You have " + attempt + " attempts remaining;");
-         if (attempt == 0) {
-             username.disabled = true;
-             password.disabled = true;
-             loginButton.disabled = true;
-             return false;
-         }
-     }
- }
+// let attempt = 3;
+// function validate() {
+//     if (username == "${username.value}" && password == "${password.value}") {
+//         window.location = "dashboard.html";
+//         return false;
+//     } else {
+//         attempt--;
+//         alert("Invalid login information. You have " + attempt + " attempts remaining;");
+//         if (attempt == 0) {
+//             username.disabled = true;
+//             password.disabled = true;
+//             loginButton.disabled = true;
+//             return false;
+//         }
+//     }
+// }
 
 const signupUsername = document.getElementById("signup-username");
 const signupEmail = document.getElementById("signup-email");
@@ -50,7 +48,7 @@ const signupButton = document.getElementById("signup-submit-btn");
 signupButton.addEventListener("click", (event) => {
   event.preventDefault();
 
-  fetch(`http://localhost:8080/login`, {
+  fetch(`http://localhost:8080/signup`, {
     method: "POST",
     body: `{"username":"${signupUsername.value}", "email":"${signupEmail.value}", "password":"${SignupPassword.value}"}`,
     credentials: "include",
@@ -65,7 +63,5 @@ signupButton.addEventListener("click", (event) => {
 });
 
 let rememberMeCheck;
-let guestButton;
 
 rememberMeCheck = document.getElementById("login-remember-me");
-guestButton = document.getElementById("guest-login-btn");
